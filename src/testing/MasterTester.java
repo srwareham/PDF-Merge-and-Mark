80653a81d0663fe9bb91981d083ba9108d6838f9
srwareham <srwareham@hotmail.com>
2013-12-23 13:10:06 -0500

    Added testing package

---
+++ src/testing/MasterTester.java
@@ -0,0 +1,14 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MasterTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
